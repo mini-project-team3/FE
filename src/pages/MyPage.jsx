f93985c62be81b75b4a1f9befc2873d0578c9188
choi <wjfekr1@naver.com>
2023-02-25 14:22:36 +0900

    비로그인 상태일 경우 로그인페이지로 튕김

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  const navigate = useNavigate();
+  const isLogin = useSelector((state) => {
+    return state.loginSlice.isLogin;
+  });
+  useEffect(() => {
+    // if (!isLogin) {
+    //   navigate("/login");
+    // }
+  }, []);
+
+  return (
+    <div>
+      <div>Title: mypage</div>
+    </div>
+  );
 };
 
 export default MyPage;
