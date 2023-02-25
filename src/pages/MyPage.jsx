fe74edc4f3ac1be9250f52b39aebb3ba53d19685
choi <wjfekr1@naver.com>
2023-02-25 15:06:14 +0900

    최신순/좋아요 모드 구현, 누르면 상세페이지로 이동 가능

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import LoadingSpinner from "../style/LoadingSpinner";
 
 const MyPage = () => {
   const navigate = useNavigate();
+  const [sortBy, setSortBy] = useState("TIME");
   const isLogin = useSelector((state) => {
     return state.loginSlice.isLogin;
   });
@@ -11,13 +15,64 @@ const MyPage = () => {
     // if (!isLogin) {
     //   navigate("/login");
     // }
-  }, []);
+  }, [sortBy]);
 
-  return (
-    <div>
-      <div>Title: mypage</div>
-    </div>
+  const { isLoading, isError, error, data, refetch } = useQuery(
+    ["getMyReviews"],
+    () => {
+      if (sortBy === "TIME") {
+        return axios.get("/api/myreviews?page=0&criteria=createdAt&sort=DESC");
+      } else if (sortBy === "LIKES") {
+        return axios.get(
+          "/api/reviews/likes?page=0&criteria=createdAt&sort=DESC"
+        );
+      }
+    }
   );
+
+  if (isLoading) {
+    return <LoadingSpinner></LoadingSpinner>;
+  }
+  if (isError) {
+    console.log(error);
+  }
+  const reviewList = data.data.reviewList;
+
+  const navArea = `<div>Title: mypage</div>
+    <div onClick={${setSortBy("TIME")}; ${refetch()};}>내가 쓴 글</div>
+    <div onClick={${setSortBy(
+      "LIKES"
+    )}; ${refetch()};}>내가 좋아요 누른 글</div>`;
+
+  if (sortBy === "TIME") {
+    return (
+      <div>
+        {navArea}
+        {reviewList?.map((item) => (
+          <div onClick={navigate("상세페이지로")}>
+            <div>{item.title}</div>
+            <div>{item.nickname}</div>
+            <div>{item.likeCount}</div>
+            <div>{item.createdAt}</div>
+          </div>
+        ))}
+      </div>
+    );
+  } else if (sortBy === "LIKES") {
+    return (
+      <div onClick={navigate("상세페이지로")}>
+        {navArea}
+        {reviewList?.map((item) => (
+          <div>
+            <div>{item.title}</div>
+            <div>{item.nickname}</div>
+            <div>{item.likeCount}</div>
+            <div>{item.createdAt}</div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default MyPage;
