a7b5d03fef9596783a5b204d82a75cea24d4c7e4
choi <wjfekr1@naver.com>
2023-02-25 14:02:52 +0900

    myPage 라우터 생성

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,4 +1,5 @@
 import Login from "../pages/Login";
+import MyPage from "../pages/MyPage";
 import Signup from "../pages/Signup";
 
 const { BrowserRouter, Routes, Route } = require("react-router-dom");
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
