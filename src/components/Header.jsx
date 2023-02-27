29ebf0048823a07fce7faf0370e1424a0378f068
leejuae1020 <leejuae1020@naver.com>
2023-02-27 12:50:02 +0900

    헤더 이미지 클릭시 홈으로 이동 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,29 +3,33 @@ import styled from "styled-components";
 import Navigation from "./Navigation";
 import Bgm from "./Bgm";
 
-// 스타일을 적용할 컴포넌트를 생성합니다.
 const StyledHeader = styled.header`
   background-color: #ffffff;
-  margin-top: 40px;
-  padding: 30px;
-  font-size: 24px;
-  font-weight: bold;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 350px;
-  background-image: url("https://i.pinimg.com/564x/b0/b2/a2/b0b2a29133dc9d249a225d105e3b6800.jpg");
-  background-repeat: no-repeat; //이미지 반복 안할것이야
-  background-position: top; //이미지 윗부분 잘리지않게 하기
+  padding: 10px;
+  height: 400px;
+  background-repeat: no-repeat;
+  background-position: top;
 `;
 
-// 기존의 Header 컴포넌트 대신에 스타일이 적용된 StyledHeader 컴포넌트를 사용합니다.
+const HeaderLink = styled.a`
+  display: block;
+  width: 100%;
+  height: 100%;
+  background-image: url("https://i.pinimg.com/564x/b0/b2/a2/b0b2a29133dc9d249a225d105e3b6800.jpg");
+  background-repeat: no-repeat;
+  background-position: top;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
 const Header = () => {
   return (
     <>
       <Bgm />
-      <StyledHeader />
+      <StyledHeader>
+        <HeaderLink href="/" />
+      </StyledHeader>
       <Navigation />
     </>
   );
