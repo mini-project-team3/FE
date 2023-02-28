00c5f450619294af4ac595c2ecdc28ab8c598708
Choi Seungho <107627925+boompeak@users.noreply.github.com>
2023-02-28 14:01:37 +0900

    깃 에러 강제 푸쉬
    ㅇ

--- src/api/reivewCards.js
+++ src/api/reivewCards.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-//리뷰카드 전체조회 api 명세에 맞게 URL작성
+//리뷰카드 전체조회
 const getRivews = async () => {
-
- const response = await axios.get(`${process.env.REACT_APP_BASEURL}/api/reviews`);
+  const response = await axios.get(
+    `${process.env.REACT_APP_BASEURL}/api/reviews`
+  );
+  console.log(response);
   return response.data;
 };
 
