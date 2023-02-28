a304690be54cadaaae0c97eae0f621f29a6e0380
leejuae1020 <leejuae1020@naver.com>
2023-02-28 16:34:24 +0900

    오타수정..

--- src/api/reviewCards.js
+++ src/api/reviewCards.js
@@ -3,7 +3,8 @@ import axios from "axios";
 //리뷰카드 전체조회 api 명세에 맞게 URL작성
 const getRivews = async () => {
   const response = await axios.get(`${process.env.REACT_APP_BASEURL}/api/reviews`);
-  console.log(response.data);
+  console.log("data는?", response.data);
+
   return response.data;
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ import Card from "react-bootstrap/Card";
 import { TbArrowsDownUp } from "react-icons/tb";
 import { useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
-import { getRivews } from "../api/reivewCards";
+import { getRivews } from "../api/reviewCards";
 import LoadingSpinner from "../style/LoadingSpinner";
 
 // 팀장님 여깁뉘다!!!!!!!!!!!!!!!!!!
@@ -14,6 +14,7 @@ function Main() {
   const navigate = useNavigate();
 
   const { isLoading, isError, data, error } = useQuery("rivews", getRivews);
+  const reviewList = data.data;
   // console.log("이data는뭘까", data);
 
   if (isLoading) {
@@ -39,18 +40,18 @@ function Main() {
     <div className="d-flex flex-column align-items-center">
       <TbArrowsDownUp style={{ fontSize: "40px", cursor: "pointer" }} variant="dark" onClick={handleSort} />
       <br />
-      {data?.map((review, id) => (
+      {reviewList.map((review) => (
         <Card
-          key={id}
+          key={review.id}
           bg="dark"
           text="white"
           style={{ width: "30rem", height: "20rem", borderRadius: "20px" }}
           className="my-2"
-          onClick={() => goToDetailPage(id)}
+          onClick={() => goToDetailPage(review.id)}
         >
           <Card.Header>{review.title}</Card.Header>
           <Card.Body>
-            <Card.Title>샬라샬라</Card.Title>
+            <Card.Title>{review.nickname}</Card.Title>
             <Card.Text>{review.content}</Card.Text>
           </Card.Body>
         </Card>
